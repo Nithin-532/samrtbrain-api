50d8f5dd9a1a8e89f8c1711a145ff2d65d449292
Nithin Kalluru <kallurunithin8771@gmail.com>
2023-06-21 13:08:51 +0530

    added changes

--- server.js
+++ server.js
@@ -12,17 +12,12 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
     host : process.env.DATABASE_HOST,
     port: 5432,
     user : process.env.DATABASE_USER,
     password : process.env.DATABASE_PW,
     database : process.env.DATABASE_DB
   }
-});
-
-db.select('*').from('users').then(data => {
-  console.log(data);
 });
 
 const app = express();
@@ -36,8 +31,8 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(3001, () => {
-  console.log('app is running on port 3001');
+app.listen(process.env.PORT || 3001, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
 })
 
 /*
