889ff1794613fea9061f4930f3aaef05a0739707
Nithin Kalluru <kallurunithin8771@gmail.com>
2023-06-23 18:42:37 +0530

    second commit

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const knex = require('knex');
 const db = knex({ 
   client: 'pg',
   connection: {
-    connectionString : process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    },
     host: process.env.DATABASE_HOST,
     port: 5432,
     user: process.env.DATABASE_USER,
